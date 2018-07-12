2185e1ad91af6525309e3493902bf6a067200c3e
lsq890410 <279159029@qq.com>
2018-07-12 23:45:35 +0800

    2

--- WxServer-mvc/src/main/java/com/lisq/wxserver/mvc/MvcControllerTest.java
+++ WxServer-mvc/src/main/java/com/lisq/wxserver/mvc/MvcControllerTest.java
@@ -1,17 +1,16 @@
 package com.lisq.wxserver.mvc;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MvcControllerTest {
 	@RequestMapping("/test2")
 	@ResponseBody
-	String home(HttpServletRequest request) {
-		return "Hello World!"+request.getParameter("user");
+	String home(@RequestParam(name="user") String user) {
+		return "Hello World!"+user;
 	}
 
 }
