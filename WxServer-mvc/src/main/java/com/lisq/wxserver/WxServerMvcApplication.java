a4648f2a9fc71b41ae421657427562f052f5fb1a
lsq890410 <279159029@qq.com>
2018-07-12 19:54:45 +0800

    first

---
+++ WxServer-mvc/src/main/java/com/lisq/wxserver/WxServerMvcApplication.java
@@ -0,0 +1,22 @@
+package com.lisq.wxserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@Controller
+public class WxServerMvcApplication {
+	@RequestMapping("/test")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(WxServerMvcApplication.class, args);
+	}
+}
---
+++ WxServer-mvc/src/main/java/com/lisq/wxserver/mvc/MvcControllerTest.java
@@ -0,0 +1,17 @@
+package com.lisq.wxserver.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MvcControllerTest {
+	@RequestMapping("/test1")
+	@ResponseBody
+	String home(HttpServletRequest request) {
+		return "Hello World!"+request.getParameter("user");
+	}
+
+}
